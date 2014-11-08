ab9df4c7672c92accde7eaf6374bb59460afa884
Blake Stacks <stacksb@msoe.edu>
2014-11-08 12:50:10 -0600

    merge

--- AndroidRelay/src/edu/msoe/smv/raspirelay/HeadsUpDisplay.java
+++ AndroidRelay/src/edu/msoe/smv/raspirelay/HeadsUpDisplay.java
@@ -11,9 +11,11 @@ package edu.msoe.smv.raspirelay;
  */
 
 import android.app.Activity;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.text.format.Formatter;
 import android.widget.TextView;
 
 import java.io.*;
@@ -32,15 +34,29 @@ public class HeadsUpDisplay extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.headsupdisplay);
 
+
         startTime = SystemClock.currentThreadTimeMillis();
 //        String hostName = "155.92.179.102";
 
         console = (TextView)findViewById(R.id.console);
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        String ip  = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        updateView("my ip: " + ip);
 
         AsyncTask task = new ServerAsyncTask();
         // TODO: unchecked call
         task.execute(this);
 
+    }
+
+    public void sendDataToWebServer(String data) throws IOException {
+        if (data == null) {
+            updateView("data null");
+        }
+        Socket socket = new Socket("155.92.69.95", 8000);
+        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+        outToServer.writeUTF("HI BLAKE!!!");
+        outToServer.close();
     }
 
     @Override
@@ -67,6 +83,13 @@ public class HeadsUpDisplay extends Activity {
             final HeadsUpDisplay display = ((HeadsUpDisplay)params[0]);
             String line = "This fucker is null";
             try {
+                ServerSocket websock = new ServerSocket(1112);
+                Socket sock = websock.accept();
+                DataOutputStream outstream = new DataOutputStream(sock.getOutputStream());
+                String outstreamdata = "faux data\r\n";
+                outstream.writeUTF(outstreamdata);
+
+
                 serverSocket = new ServerSocket(1111);
                 Socket echoSocket = serverSocket.accept();
                 if (echoSocket == null) {
@@ -75,7 +98,8 @@ public class HeadsUpDisplay extends Activity {
                     while (SystemClock.currentThreadTimeMillis() < startTime + 30000) {
                         line = new BufferedReader(new InputStreamReader(echoSocket.getInputStream())).readLine();
                         // line can be null for end of message, let's fix that and
-                        final String output  = line = line != null ? line : "";
+                        final String output = line = line != null ? line : "";
+
                         // update ui
                         if (!line.isEmpty()) {
                             runOnUiThread(new Runnable() {
@@ -84,14 +108,23 @@ public class HeadsUpDisplay extends Activity {
                                     display.updateView(output);
                                 }
                             });
+                            outstreamdata += output;
                         }
                     }
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            display.updateView("done receiving - now sent to blake");
+                        }
+                    });
                 }
+                // send to web server
+                outstream.writeUTF(outstreamdata + "\r\n");
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
             return null;
         }
-
     }
 }
