69ed1c50c6b9332492d6aed05fe7f47a223fcbce
Austin Hartline <hartlinea@msoe.edu>
2014-12-13 10:24:28 -0600

    created classes for encapsulating some behaviors

--- AndroidRelay/src/edu/msoe/smv/raspirelay/HeadsUpDisplay.java
+++ AndroidRelay/src/edu/msoe/smv/raspirelay/HeadsUpDisplay.java
@@ -11,6 +11,7 @@ package edu.msoe.smv.raspirelay;
  */
 
 import android.app.Activity;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.format.Formatter;
@@ -21,12 +22,13 @@ import android.widget.TextView;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class HeadsUpDisplay extends Activity {
 
     public TextView console;
-    public CheckedTextView webConnected,piConnected;
-    private ServerSocket piServerSocket,webServerSocket ;
+    public CheckedTextView webConnected, piConnected;
+    private ServerSocket piServerSocket, webServerSocket ;
     public WebListener myWebListener;
     public PiListener myPiListener;
     public boolean isPiDataServerRunning=true,isWebDataServerRunning=true;
@@ -52,33 +54,118 @@ public class HeadsUpDisplay extends Activity {
         updateView("my ip: " + ip);
 
         //get connection status displays
-        webConnected=(CheckedTextView)findViewById(R.id.webConnected);
-        piConnected=(CheckedTextView)findViewById(R.id.piConnected);
+        webConnected = (CheckedTextView)findViewById(R.id.webConnected);
+        piConnected = (CheckedTextView)findViewById(R.id.piConnected);
 
-        /*AsyncTask task = new ServerAsyncTask();
-        // TODO: unchecked call
-        task.execute(this);*/
-
-        /*myPiListener=new PiListener();
-        myPiListener.execute();*/
+//        myPiListener = new PiListener();
+//        myPiListener.execute();
     }
-    //onClick method for the startWebListener button
+
+
+    /**
+     * onClick method for the startWebListener button
+     * @param v
+     */
     public void startWebClick(View v){
         startWebServerListener();
     }
-    //onClick method for the stopWebListener button
+
+    /**
+     * onClick method for the stopWebListener button
+     * @param v
+     */
     public void stopWebClick(View v){
         stopWebServerListener();
     }
-    //onClick method for the startPiListener button
+
+    /**
+     * onClick method for the startPiListener button
+     * @param v
+     */
     public void startPiClick(View v){
         startPiListener();
     }
-    //onClick method for the stopPiListener button
+
+    /**
+     * onClick method for the stopPiListener button
+     * @param v
+     */
     public void stopPiClick(View v){
         stopPiListener();
     }
 
+    /**
+     *
+     */
+    public void startWebServerListener(){
+        if (myWebListener != null)
+            stopWebServerListener();
+        isWebDataServerRunning=true;
+        myWebListener=new WebListener();
+        myWebListener.execute();
+    }
+
+    /**
+     *
+     */
+    public void stopWebServerListener(){
+        try {
+            webServerSocket.close();
+            webServerOutput.close();
+        } catch(Exception e) {
+            updateView("Error: "+e.toString());
+        }
+        isWebDataServerRunning=false;
+        myWebListener=null;
+        setWebConnected(false);
+        updateView("--WebListener stopped--");
+    }
+
+    /**
+     *
+     */
+    public void startPiListener(){
+        if (myPiListener != null)
+            stopPiListener();
+        isPiDataServerRunning = true;
+        myPiListener = new PiListener();
+        myPiListener.execute();
+    }
+
+    /**
+     *
+     */
+    public void stopPiListener(){
+        try {
+            piServerSocket.close();
+        } catch(Exception e) {
+            updateView("Error: "+e.toString());
+        }
+        isPiDataServerRunning = false;
+        myPiListener = null;
+        setPiConnected(false);
+        updateView("--PiListener stopped--");
+    }
+
+    /**
+     *
+     * @param val
+     */
+    public void setWebConnected(boolean val){
+        webConnected.setChecked(val);
+    }
+
+    /**
+     *
+     * @param val
+     */
+    public void setPiConnected(boolean val){
+        piConnected.setChecked(val);
+    }
+
+    /**
+     *
+     */
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -92,37 +179,54 @@ public class HeadsUpDisplay extends Activity {
             }
         }
     }
-    //update the console text display widget with the specified text
+
+    /**
+     * update the console text display view with the specified text
+     * @param line the line of text to write to the console
+     */
     public void updateView(String line) {
         try {
-            //append the string to the text of the consoel
             console.append(line + "\n");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(console);
             console.append(e.toString());
         }
     }
 
-    //instantiates the webServerOutput Buffered Writer
-    public void instantiateWriter(OutputStream stream){
+    /**
+     * instantiates the webServerOutput Buffered Writer
+     * @param stream
+     */
+    public void instantiateWriter(OutputStream stream) {
         webServerOutput = new BufferedWriter(new OutputStreamWriter(stream));
     }
 
-    //WebServer Listener Task for asynchronous communcation between the phone and the PitView Web Server
-    public class WebListener extends AsyncTask{
-        //pendingData is the message queue string
-        String pendingData="";
+    /**
+     * WebServer Listener Task for asynchronous communication between the phone and the PitView Web Server
+     *
+     * java why u make us use void as a type :-(
+     */
+    private class WebListener extends AsyncTask<Void, Void, Void> {
 
-        //appends S to the message queue
-        public void sendData(String s){
+        // the message queue string
+        String pendingData = "";
+
+        /**
+         * appends the string to the message queue
+         * @param s
+         */
+        public void sendData(String s) {
             //make sure there's no empty line if it's the first message in the queue
-            if(pendingData.equals(""))
-                pendingData=s;
+            if (pendingData.equals(""))
+                pendingData = s;
             else
-                pendingData+="\n"+s;
+                pendingData += s + "\n";
         }
-        //Updates the console text with the specified string
-        public void updateUI(final String s){
+
+        /**
+         * updates the console text with the specified string
+         */
+        public void updateUI(final String s) {
             //cannot access UI stuff on a non-UI thread
             runOnUiThread(new Runnable() {
                 @Override
@@ -131,8 +235,11 @@ public class HeadsUpDisplay extends Activity {
                 }
             });
         }
-        //updates the Web Listener conenction indicator with the specified value
-        public void updateConnected(final boolean val){
+
+        /**
+         * updates the Web Listener concoction indicator with the specified value
+         */
+        public void updateConnected(final boolean val) {
             //cannot access UI stuff on a non-UI thread
             runOnUiThread(new Runnable() {
                 @Override
@@ -141,8 +248,11 @@ public class HeadsUpDisplay extends Activity {
                 }
             });
         }
-        //stops the web server
-        public void bail(){
+
+        /**
+         * stops the web server
+         */
+        public void bail() {
             //cannot access UI stuff on a non-UI thread
             runOnUiThread(new Runnable() {
                 @Override
@@ -152,13 +262,20 @@ public class HeadsUpDisplay extends Activity {
             });
         }
 
-        //task's main method, initiates connection/writer/sockets, continuous while loop
-        //overrides the default doInBackground method
+        /**
+         * task's main method, initiates connection/writer/sockets, continuous while loop
+         * overrides the default doInBackground method
+         * @param params an arbitrary number (0..*) of Void objects.
+         *               Yep, void freaking objects. don't pass this anything.
+         * @return void.
+         *      yes, you can in fact return void type objects in java. don't do this in graded code.
+         */
         @Override
-        protected Object doInBackground(Object[] params) {
-            try{
+        protected Void doInBackground(Void... params) {
+            try {
                 //update the console display
                 updateUI("Starting WebListener");
+
                 //instantiate the server socket for PitView to connect to on port 1112
                 webServerSocket = new ServerSocket(1112);
                 //instantiate the connection with the PitView web server as the accepted socket from the PitView connection request
@@ -168,13 +285,13 @@ public class HeadsUpDisplay extends Activity {
                 updateConnected(true);
                 //welcome message
                 updateUI("PitView v1.0");
-                updateUI("Connected to PitView (IP=" + webServerDataSocket.getRemoteSocketAddress().toString().substring(1) + ":1112)");
+                updateUI("Connected to PitView (IP = " + webServerDataSocket.getRemoteSocketAddress().toString().substring(1) + ":1112)");
                 //instantiate the data writer to relay the data to PitView
                 instantiateWriter(webServerDataSocket.getOutputStream());
                 //add data to the message queue
                 sendData("Connection initialized");
                 //repeat while server is on
-                while(isWebDataServerRunning){
+                while (isWebDataServerRunning){
                     //if queue isn't empty
                     if(!pendingData.equals("")) {
                         //write the data
@@ -187,20 +304,26 @@ public class HeadsUpDisplay extends Activity {
                         pendingData = "";
                     }
                 }
-            }catch(SocketException se) {
+            } catch (SocketException se) {
                 //connection was interrupted, closed on one end, or nullified
                 updateUI("WebServer connection interrupted, shutting down...");
                 bail();
-            }catch(Exception e){
-                updateUI("Error: "+e.toString());
+            } catch (Exception e) {
+                updateUI("Error: " + e.toString());
             }
             return null;
         }
     }
 
-    //WebServer Listener Task for asynchronous communcation between the phone and the PitView Web Server
-    public class PiListener extends AsyncTask{
-        //Updates the console text with the specified string
+    /**
+     * WebServer Listener Task for asynchronous communcation between the phone and the PitView Web Server
+     */
+    public class PiListener extends AsyncTask<Void, Void, Void> {
+
+        /**
+         * Updates the console text with the specified string
+         * @param s
+         */
         public void updateUI(final String s){
             //cannot access UI stuff on a non-UI thread
             runOnUiThread(new Runnable() {
@@ -210,7 +333,11 @@ public class HeadsUpDisplay extends Activity {
                 }
             });
         }
-        //updates the Web Listener conenction indicator with the specified value
+
+        /**
+         * updates the Web Listener conenction indicator with the specified value
+         * @param val
+         */
         public void updateConnected(final boolean val){
             //cannot access UI stuff on a non-UI thread
             runOnUiThread(new Runnable() {
@@ -220,8 +347,11 @@ public class HeadsUpDisplay extends Activity {
                 }
             });
         }
-        //stops the web server
-        public void bail(){
+
+        /**
+         * stops the web server
+         */
+        public void bail() {
             //cannot access UI stuff on a non-UI thread
             runOnUiThread(new Runnable() {
                 @Override
@@ -231,11 +361,23 @@ public class HeadsUpDisplay extends Activity {
             });
         }
 
-        //task's main method, initiates connection/reader/sockets, continuous while loop
-        //overrides the default doInBackground method
+        /**
+         * task's main method, initiates connection/reader/sockets, continuous while loop
+         * overrides the default doInBackground method
+         * @param params
+         * @return
+         */
         @Override
-        protected Object doInBackground(Object[] params) {
+        protected Void doInBackground(Void... params) {
             try {
+
+                /**
+                 * raspberry pi will act as the server, we are the client
+                 *
+                 * !!1!
+                 */
+
+
                 //update the console display
                 updateUI("Starting PiListener");
                 //instantiate the server socket for the pi to connect to on port 1111
@@ -251,7 +393,7 @@ public class HeadsUpDisplay extends Activity {
                     //while the server is running
                     while (isPiDataServerRunning) {
                         //read a line of the message
-                        String line= new BufferedReader(new InputStreamReader(echoSocket.getInputStream())).readLine();
+                        String line = new BufferedReader(new InputStreamReader(echoSocket.getInputStream())).readLine();
                         // line can be null for end of message, let's fix that and
                         final String output = line = line != null ? line : "";
 
@@ -264,84 +406,15 @@ public class HeadsUpDisplay extends Activity {
                     }
                     updateUI("done receiving - now sent to blake");
                 }
-            }catch(SocketException se) {
+            } catch(SocketException se) {
                 //connection was interrupted, closed on one end, or nullified
                 updateUI("Pi connection interrupted, shutting down...");
                 bail();
-            }catch(Exception e){
-                updateUI("Error: "+e.toString());
+            } catch(Exception e){
+                updateUI("Error: " + e.toString());
             }
+
             return null;
         }
     }
-
-    /*---Below is old code---*/
-
-    /*public class ServerAsyncTask extends AsyncTask {
-        public void updateUI(final String s){
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    updateView(s);
-                }
-            });
-        }
-        @Override
-        public Object doInBackground(final Object... params) {
-            final HeadsUpDisplay display = ((HeadsUpDisplay)params[0]);
-            String line = "This fucker is null";
-            try {
-                webServerSocket = new ServerSocket(1112);
-                webServerDataSocket = webServerSocket.accept();
-                updateUI("PitView v1.0");
-                updateUI("Connected to PitView (IP=" + webServerDataSocket.getRemoteSocketAddress().toString().substring(1) + ":1112)");
-                instantiateWriter(webServerDataSocket.getOutputStream());
-                String outstreamdata = "faux data\r\n";
-                sendDataToWebServer("testing web server1", webServerOutput);
-                //webServerOutput.flush();*/
-                /*webServerOutput.write(outstreamdata);
-                webServerOutput.flush();
-                webServerOutput.write(outstreamdata);
-                webServerOutput.flush();
-                webServerOutput.flush();
-                webServerOutput.write(outstreamdata);
-                webServerOutput.flush();
-                webServerOutput.write(outstreamdata);*/
-                /*
-                piServerSocket = new ServerSocket(1111);
-                Socket echoSocket = piServerSocket.accept();
-                if (echoSocket == null) {
-                    updateUI("Socket connection failed");
-                } else {
-                    while (SystemClock.currentThreadTimeMillis() < startTime + 30000) {
-                        line = new BufferedReader(new InputStreamReader(echoSocket.getInputStream())).readLine();
-                        // line can be null for end of message, let's fix that and
-                        final String output = line = line != null ? line : "";
-
-                        // update ui
-                        if (!line.isEmpty()) {
-                            updateUI(output);
-                            outstreamdata += output;
-                        }
-                    }
-                    updateUI("done receiving - now sent to blake");
-                }
-                // send to web server
-                webServerOutput.write((outstreamdata + "\r\n"));
-                webServerOutput.flush();
-
-                sendDataToWebServer("testing web server", webServerOutput);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-        public void sendDataToWebServer(String data,BufferedWriter stream) throws IOException {
-            if (data == null) {
-                updateUI("data null");
-            }
-            stream.write(data);
-            stream.flush();
-        }
-    }*/
 }
---
+++ AndroidRelay/src/edu/msoe/smv/raspirelay/VehicalConnectionAgent.java
@@ -0,0 +1,28 @@
+/**
+ * Project: AndroidRelay
+ * Author: Austin Hartline
+ * Date: 12/7/14
+ */
+
+package edu.msoe.smv.raspirelay;
+
+import android.os.AsyncTask;
+
+/**
+ * @author austin
+ * @version 2014.12.07
+ */
+public class VehicalConnectionAgent extends AsyncTask<Void, Void, Void> {
+
+    /**
+     * bring the code for the raspberry pi connector here
+     *
+     * this pushes a connection to the raspberry pi (I think)
+     * maybe return a socket?
+     */
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+}
