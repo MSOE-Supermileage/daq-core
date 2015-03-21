3d2fd8e64b4aedd6dfac67e2cfd8e2827517ea2d
Matthew Mahnke <mahnkem@msoe.edu>
2015-03-21 15:18:18 -0500

    Added toCSV() method for DataNodes

--- Raspberry Pi/src/edu/msoe/smv/raspi/DataNode.java
+++ Raspberry Pi/src/edu/msoe/smv/raspi/DataNode.java
@@ -33,19 +33,31 @@ public class DataNode {
 	 * Returns a string representation of this node formatted in JSON.
 	 * </p>
 	 * Pretty printing is used, that is the resulting string spans multiple lines. For example: </p>
-	 * <code>{
+	 * <pre>{
 	 *     "altitude": 5.0,
 	 *     "batteryVoltage": 2.0,
 	 *     "date": "Oct 25, 2014 2:04:23 PM",
 	 *     "latitude": 3.0,
 	 *     "longitude": 5.0,
 	 *     "rpm": 1.0
-	 * }</code>
+	 * }</pre>
 	 *
 	 * @return a string representation of this node formatted in JSON
 	 */
 	@Override
 	public String toString() {
 		return new GsonBuilder().setPrettyPrinting().create().toJson(this);
+	}
+
+	/**
+	 * Returns a string representation of this DataNode as comma-separated values.
+	 * <p/>
+	 * Example result: <pre>12345678,200,20</pre> where the first value is the Unix time, the second is the RPM, and
+	 * the third is the speed.
+	 *
+	 * @return a string representation of this DataNode as comma-separated values
+	 */
+	public String toCSV() {
+		return date.getTime() + "," + rpm + "," + speed;
 	}
 }
