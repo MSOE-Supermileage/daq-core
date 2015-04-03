b33596da8830fcff445eec71e0eeb1163b5d3075
Matthew Mahnke <mahnkem@msoe.edu>
2015-04-03 18:32:00 -0500

    Temporary Main.
    This does do close to what it should be doing, but not quite exactly.

---
+++ Raspberry Pi/src/edu/msoe/smv/raspi/Main.java
@@ -0,0 +1,88 @@
+package edu.msoe.smv.raspi;
+
+import com.pi4j.io.gpio.PinState;
+import edu.msoe.smv.VehicleAttributes;
+import edu.msoe.smv.raspi.sensors.RotationalSpeedSensor;
+
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by matt on 3/21/15.
+ */
+public class Main {
+	/**
+	 * TODO
+	 */
+	private static VehicleAttributes vehicle;
+
+	public static void main(String[] args) {
+		// set VehicleAttributes vehicle
+		if (args.length > 0) {
+			setVehicle(args[0]);
+		}
+		System.out.println(vehicle);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Run things
+		final List<DataNode> nodeList = Collections.synchronizedList(new LinkedList<DataNode>());
+		try {
+			Server server = Server.getInstance(nodeList);
+			System.out.println("Made server");
+
+			final RotationalSpeedSensor rotationalSpeedSensor = new RotationalSpeedSensor(7, PinState.HIGH, 1);
+			System.out.println("Made rot speed sensor");
+			Thread getDatas = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					while (true) {
+						double foo = rotationalSpeedSensor.getValue();
+//						foo = Math.random();
+						System.out.println("got omega value");
+						nodeList.add(new DataNode(foo, vehicle.getTireDiam() * foo));
+						System.out.println("added data node");
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			});
+			System.out.println("Made data thread");
+
+			Thread serverThread = new Thread(server);
+			server.enableServer(true);
+			getDatas.start();
+			System.out.println("Started data thread");
+			serverThread.start();
+			System.out.println("Started server thread");
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Sets {@link #vehicle} to the correct {@link VehicleAttributes} enum.
+	 * <p/>
+	 * {@code vehicleName} should be the name of the vehicle that this is running on, for example "MP82", "MP680", or
+	 * "MP252".
+	 *
+	 * @param vehicleName the name of the vehicle
+	 */
+	private static void setVehicle(String vehicleName) {
+		if (vehicleName.contains("82")) {
+			vehicle = VehicleAttributes.MP82;
+		} else if (vehicleName.contains("680")) {
+			vehicle = VehicleAttributes.MP680;
+		} else {
+			vehicle = VehicleAttributes.MP252;
+		}
+	}
+}
