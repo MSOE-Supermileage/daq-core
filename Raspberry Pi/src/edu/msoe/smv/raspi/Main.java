4c1b28a14c11d7a55a7812726505f441a825c5c8
Matthew Mahnke <mahnkem@msoe.edu>
2015-04-04 16:00:59 -0500

    Logging locally to a file works...
    Time to clean it up.

--- Raspberry Pi/src/edu/msoe/smv/raspi/Main.java
+++ Raspberry Pi/src/edu/msoe/smv/raspi/Main.java
@@ -1,13 +1,18 @@
 package edu.msoe.smv.raspi;
 
-import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+import com.pi4j.wiringpi.Gpio;
 import edu.msoe.smv.VehicleAttributes;
+import edu.msoe.smv.raspi.sensors.DataCollector;
 import edu.msoe.smv.raspi.sensors.RotationalSpeedSensor;
 
 import java.net.SocketException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 /**
  * Created by matt on 3/21/15.
@@ -25,48 +30,60 @@ public class Main {
 		} else {
 			System.out.println("You must specify which vehicle this is being run on.");
 			System.out.println("For example:");
-			System.out.println("\tsudo pi4j -r Raspberry_Pi.jar MP82");
 			System.out.println("\tsudo java -jar Raspberry_Pi.jar MP82");
 			System.exit(1);
 		}
 		System.out.println(vehicle);
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
 		// Run things
-		final List<DataNode> nodeList = Collections.synchronizedList(new LinkedList<DataNode>());
+		final List<DataNode> nodeList = new LinkedList<DataNode>();
 		try {
 			Server server = Server.getInstance(nodeList);
 			System.out.println("Made server");
 
+			GpioController gpio = GpioFactory.getInstance();
+
+			/*GpioPinDigitalInput counterGpio =
+					gpio.provisionDigitalInputPin(RaspiPin.GPIO_07, PinPullResistance.PULL_UP);
+			counterGpio.addTrigger(new GpioCallbackTrigger(PinState.HIGH, new Callable<Void>() {
+				@Override
+				public Void call() throws Exception {
+					System.out.println("I found the magnet!");
+					return null;
+				}
+			}));*/
+
+//			final GpioPinDigitalOutput ledOut = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
+//			final GpioPinDigitalInput foo = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07, PinPullResistance.PULL_DOWN);
+//			foo.addTrigger(new GpioSyncStateTrigger(ledOut));
+
 			final RotationalSpeedSensor rotationalSpeedSensor = new RotationalSpeedSensor(7, PinState.HIGH, 1);
+			rotationalSpeedSensor.setNodeList(nodeList);
 			System.out.println("Made rot speed sensor");
-			Thread getDatas = new Thread(new Runnable() {
+			/*Thread getDatas = new Thread(new Runnable() {
 				@Override
 				public void run() {
+					double tireDiam = vehicle.getTireDiam();
 					while (true) {
 						double foo = rotationalSpeedSensor.getValue();
-//						foo = Math.random();
+						foo = Math.random();
 						System.out.println("got omega value");
-						nodeList.add(new DataNode(foo, vehicle.getTireDiam() * foo));
-						System.out.println("added data node");
-						try {
-							Thread.sleep(100);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
+						DataNode dn = new DataNode(foo, tireDiam * foo);
+						nodeList.add(dn);
 					}
 				}
 			});
-			System.out.println("Made data thread");
+			System.out.println("Made data thread");*/
 
 			Thread serverThread = new Thread(server);
 			server.enableServer(true);
-			getDatas.start();
-			System.out.println("Started data thread");
+//			getDatas.start();
+//			System.out.println("Started data thread");
 			serverThread.start();
 			System.out.println("Started server thread");
 		} catch (SocketException e) {
