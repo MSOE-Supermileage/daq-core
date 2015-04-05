55f17939f5dc37540d17a215e281a22112a06eb2
Matthew Mahnke <mahnkem@msoe.edu>
2015-04-04 23:23:19 -0500

    Add counter to DataNodes that count how the number of revolutions

--- Raspberry Pi/src/edu/msoe/smv/raspi/DataNode.java
+++ Raspberry Pi/src/edu/msoe/smv/raspi/DataNode.java
@@ -20,20 +20,33 @@ public class DataNode {
 	 * The speed at this DataNode
 	 */
 	private final double speed;
+	/**
+	 * Counts the number of DataNodes created.
+	 * <p/>
+	 * This is used to track how far the vehicle has gone.
+	 */
+	private static long count = 0;
 
 	/**
 	 * Constructs a new DataNode
-	 * @param rpm the RPM at this DataNode
-	 * @param speed the speed at this DataNode
+	 *
+	 * @param rpm      the RPM at this DataNode
+	 * @param speed    the speed at this DataNode
+	 * @param realData false if this DataNode is a dummy DataNode
 	 */
-	public DataNode(double rpm, double speed) {
+	public DataNode(double rpm, double speed, boolean realData) {
 		this.rpm = rpm;
 		this.speed = speed;
 		this.unixTime = System.currentTimeMillis();
+
+		if (realData) {
+			count++;
+		}
 	}
 
 	/**
 	 * Returns the UNIX epoch time when this DataNode was created
+	 *
 	 * @return the UNIX epoch time when this DataNode was created
 	 */
 	public long getUnixTime() {
@@ -42,6 +55,7 @@ public class DataNode {
 
 	/**
 	 * Returns the RPM at this DataNode
+	 *
 	 * @return the RPM at this DataNode
 	 */
 	public double getRpm() {
@@ -50,6 +64,7 @@ public class DataNode {
 
 	/**
 	 * Returns the speed at this DataNode
+	 *
 	 * @return the speed at this DataNode
 	 */
 	public double getSpeed() {
@@ -57,16 +72,13 @@ public class DataNode {
 	}
 
 	/**
-	 * Returns a string representation of this node formatted in JSON.
-	 * </p>
-	 * Pretty printing is used, that is the resulting string spans multiple lines. For example: </p>
+	 * Returns a string representation of this node formatted in JSON. </p> Pretty printing is used, that is the
+	 * resulting string spans multiple lines. For example: </p>
 	 * <pre>{
-	 *     "altitude": 5.0,
-	 *     "batteryVoltage": 2.0,
 	 *     "unixTime": 1414263863,
-	 *     "latitude": 3.0,
-	 *     "longitude": 5.0,
-	 *     "rpm": 1.0
+	 *     "rpm": 1.0,
+	 *     "speed": 2.0,
+	 *     "count": 23
 	 * }</pre>
 	 *
 	 * @return a string representation of this node formatted in JSON
@@ -85,6 +97,6 @@ public class DataNode {
 	 * @return a string representation of this DataNode as comma-separated values
 	 */
 	public String toCSV() {
-		return unixTime + "," + rpm + "," + speed;
+		return unixTime + "," + rpm + "," + speed + "," + count;
 	}
 }
